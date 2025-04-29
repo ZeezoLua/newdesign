4b845e364a669f5e0a25a974cf703cfb7b3d4fdb
ZeezoLua <gwertxyt@gmail.com>
2025-04-29 23:16:23 +0300

    new upd

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,21 +1,24 @@
-
+import { BiMenu } from "react-icons/bi"
 
 function Navbar() {
-  return (
-    <div>
-        <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-            <div className="text-white text-lg font-bold">My Website</div>
-            <ul className="flex space-x-4">
-                <li><a href="#" className="text-gray-300 hover:text-white">Home</a></li>
-                <li><a href="#" className="text-gray-300 hover:text-white">About</a></li>
-                <li><a href="#" className="text-gray-300 hover:text-white">Services</a></li>
-                <li><a href="#" className="text-gray-300 hover:text-white">Contact</a></li>
-            </ul>
-            </div>
-        </nav>
-    </div>
-  )
+    return (
+        <div>
+            <nav className="bg-emerald-700 h-16 flex justify-center items-center px-4">
+                <div className="container mx-auto flex justify-between items-center">
+                    <div className="text-white text-lg font-bold">My Website</div>
+                    <ul className="hidden md:flex space-x-4">
+                        <li><a href="#" className="link transition-all duration-500">Home</a></li>
+                        <li><a href="#" className="link transition-all duration-500">About</a></li>
+                        <li><a href="#" className="link transition-all duration-500">Services</a></li>
+                        <li><a href="#" className="link transition-all duration-500">Contact</a></li>
+                    </ul>
+                    <div className="flex md:hidden justify-center items-center">
+                        <BiMenu size={32} />
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline px-12 py-4">
         Hello world!
       </h1>
-      <p className="text-lg">
+      <p className="text-lg px-12">
         This is a simple Next.js app with Tailwind CSS.
       </p>
     </>
